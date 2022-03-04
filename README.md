a97d4d31a8360c800e860fd3687d41795b3b0116
Duo Pan <pd54007@gmail.com>
2022-03-04 23:06:45 +1100

    highlight the current tab.

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # Powerball history data
 
+## Functions
 #### 显示历史数据
 #### 显示前7球区间
 #### 号码分布图
@@ -7,9 +8,11 @@
 #### 统计数据
 #### PB包含在前面
 
+## Other Styles
+##### 高亮当前tab
+
 # To do (features for predicting)
 - self-define a group of numbers, add it into the tables
 - 数字出现频率的可视化
 - 数字多久未出现的可视化
-- hight current tab
 - predict the number or condition by each rule
--- utils.js
+++ utils.js
@@ -1,3 +1,11 @@
 export const removeData = () => {
     document.querySelectorAll(".remove").forEach(el => el.remove());
 };
+
+export const highlightBtn = (id) => {
+    const menuBtns = document.getElementsByClassName("menuBtn");
+    // menuBtns.forEach(btn => btn.style.backgroundColor = 'red');
+    for (const btn of menuBtns) {
+        btn.style.backgroundColor = btn.id === id ? '#00ea4e' : '';
+    }
+};
